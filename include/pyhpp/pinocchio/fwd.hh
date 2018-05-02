03ea65ec41a93a1696e30ed146f331ffe313b542
Joseph Mirabel <jmirabel@laas.fr>
2018-05-02 17:56:29 +0200

    Add minimal bindings for Liegroup space and element

--- include/pyhpp/pinocchio/fwd.hh
+++ include/pyhpp/pinocchio/fwd.hh
@@ -23,6 +23,7 @@
 namespace pyhpp {
   namespace pinocchio {
     void exposeDevice ();
+    void exposeLiegroup ();
   }
 }
 
--- src/pyhpp/pinocchio/liegroup.cc
+++ src/pyhpp/pinocchio/liegroup.cc
@@ -17,15 +17,30 @@
 // hpp-python  If not, see
 // <http://www.gnu.org/licenses/>.
 
+#include <pyhpp/pinocchio/urdf/fwd.hh>
+
 #include <boost/python.hpp>
 
-#include <pyhpp/pinocchio/fwd.hh>
+#include <eigenpy/eigenpy.hpp>
+
+#include <hpp/pinocchio/liegroup-space.hh>
+#include <hpp/pinocchio/liegroup-element.hh>
 
 using namespace boost::python;
 
-BOOST_PYTHON_MODULE(pyhpppinocchio)
-{
-  import ("pinocchio");
-  pyhpp::pinocchio::exposeDevice();
-  pyhpp::pinocchio::exposeLiegroup();
+namespace pyhpp {
+  namespace pinocchio {
+    using namespace hpp::pinocchio;
+
+    void exposeLiegroup()
+    {
+      class_<LiegroupSpace, LiegroupSpacePtr_t, boost::noncopyable> ("LiegroupSpace", no_init)
+        .def ("name", &LiegroupSpace::name, return_value_policy<return_by_value>())
+        ;
+      class_<LiegroupElement> ("LiegroupElement", init<const vector_t&, const LiegroupSpacePtr_t&>())
+        .def ("vector", static_cast <const vector_t& (LiegroupElement::*) () const> (&LiegroupElement::vector), return_value_policy<return_by_value>())
+        .def ("space", &LiegroupElement::space, return_value_policy<return_by_value>())
+        ;
+    }
+  }
 }
