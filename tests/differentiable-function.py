c4edbfd6c651cab7c278c70533e75795fe406ec9
Joseph Mirabel <jmirabel@laas.fr>
2018-05-03 15:15:15 +0200

    Add unit tests

---
+++ tests/differentiable-function.py
@@ -0,0 +1,32 @@
+from pyhpp.pinocchio import LiegroupElement, LiegroupSpace
+
+from pyhpp.constraints import DifferentiableFunction
+import numpy as np
+
+class Function (DifferentiableFunction):
+    def __init__ (self):
+        super(Function, self).__init__ (2,2,2,"Test")
+    def impl_compute (self, res, arg):
+        res.v = 2 * arg
+    def impl_jacobian (self, res, arg):
+        res = 2 * np.eye(2)
+        # print res
+        return res
+
+function = Function ()
+print function
+
+q = np.ones((function.ni,1))
+
+# C++ API
+v = LiegroupElement (function.outputSpace())
+function.value(v, q)
+print v
+
+J  = np.zeros((function.ndo,function.ndi))
+function.jacobian (J, q)
+print J
+
+# Pythonic API
+v = function(q)
+J = function.J (q)
