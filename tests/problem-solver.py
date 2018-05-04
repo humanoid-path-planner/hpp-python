72b4bc7eb534ecbc19445454a857657fbed60093
Joseph Mirabel <jmirabel@laas.fr>
2018-05-04 19:11:47 +0200

    Update unit tests

---
+++ tests/problem-solver.py
@@ -0,0 +1,30 @@
+from pyhpp.pinocchio import urdf
+from pyhpp.core import ProblemSolver
+import pyhpp.core.path
+import pinocchio, eigenpy
+import numpy as np
+
+ps = ProblemSolver.create()
+robot = ps.createRobot('ur3')
+urdf.loadRobotModel (robot, "anchor", "ur_description", "ur3", "_gripper", "_gripper")
+ps.robot(robot)
+
+qinit = np.zeros ((robot.model().nq, 1))
+qgoal = qinit.copy()
+qgoal[0,0] = 1
+
+ps.initConfig (qinit)
+ps.addGoalConfig (qgoal)
+ps.solve ()
+
+paths = ps.paths()
+path = paths.get(0)
+
+assert (path.initial() == qinit).all()
+
+q, success = path(0.3)
+assert success
+
+qq = np.zeros_like(qinit)
+success = path(qq, path.length())
+assert success
