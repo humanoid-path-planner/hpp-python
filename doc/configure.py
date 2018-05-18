f3a1bd89db138f7f97806c218e857a60ceac9d43
Joseph Mirabel <jmirabel@laas.fr>
2018-05-18 14:52:58 +0200

    Generate documentation from doxygen-xml folder.

---
+++ doc/configure.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python2.7
+
+from __future__ import print_function
+import re,os,sys
+from doxygen_xml_parser import Index
+
+class WrongIndex:
+    def __init__ (self, msg):
+        self.msg = msg
+    def classDoc (self, name):
+        return WrongClassDoc (self.msg, name)
+
+class WrongClassDoc:
+    def __init__ (self, msg, name):
+        self.msg = msg
+        self.name = name
+
+    def _getMsg (self, el):
+        return self.msg + " " + el + " is not documented"
+
+    def getClassDoc (self):
+        return self._getMsg(self.name), ""
+
+    def getClassMemberDoc (self, member):
+        return self._getMsg(self.name + "::" + member), ""
+
+    def getClassMethodDoc (self, method):
+        return self._getMsg(self.name + "::" + method), "", []
+
+def _xmlDirFromPkgConfig (pkg):
+    path = os.popen("pkg-config --variable=docdir " + pkg).read().strip()
+    path = os.path.join (path, "doxygen-xml") 
+    if not os.path.isdir(path):
+      return WrongIndex("Cannot find doxygen-xml for package " + pkg + ".")
+    return path
+
+nsToPackage= {
+    "hpp::core"       : _xmlDirFromPkgConfig ("hpp-core")                      ,
+    "hpp::constraints": _xmlDirFromPkgConfig ("hpp-constraints")
+    }
+
+def indexFromNamespace (ns):
+  if nsToPackage.has_key(ns):
+    path = nsToPackage[ns]
+    if isinstance(path, str):
+        return Index (os.path.join (path, "index.xml"))
+    else:
+        return path
+  raise ValueError ("Unknown namespace " + ns)
+
+def escape (s):
+  # return s.replace ('\n', r'\n')
+  return s.replace ('\n', r'\n')
+
+def make_doc_string (brief, detailled):
+  if len(brief) == 0 or brief.isspace():
+    return '"' + detailled + '"'
+  return '"' + brief + "\n" + detailled + '"'
+
+def make_args_string (args):
+  return '(' + ",".join (['arg("'+a+'")' for a in args]) + ')'
+
+def substitute (istr, ostr):
+  nsPattern = re.compile (r"DocNamespace\s*\(\s*(?P<namespace>[\w:]+)\s*\)")
+  classPattern = re.compile (r"DocClass\s*\(\s*(?P<class>[\w:]+)\s*\)")
+  dcmPattern = re.compile (r"DocClassMethod\s*\(\s*(?P<method>[\w:]+)\s*(,\s*(?P<class>[\w:]+)\s*)?\)")
+  classDoc = None
+  for line in map (lambda s: s.rstrip(), istr):
+    for match in nsPattern.finditer (line):
+      currentNamespace = match.group("namespace")
+      index = indexFromNamespace (currentNamespace)
+    for match in classPattern.finditer (line):
+      currentClass = match.group("class")
+    for match in dcmPattern.finditer (line):
+      if match.group("class") is not None:
+        cn = currentNamespace + "::" + match.group("class")
+      else:
+        cn = currentNamespace + "::" + currentClass
+      mn = match.group("method")
+      try:
+        if classDoc is None or classDoc.classname != cn:
+          classDoc = index.classDoc (cn)
+        b, d, args = classDoc.getClassMethodDoc (mn)
+        line = line.replace (match.group(0),
+                escape(make_doc_string(b, d)) + ", "
+                + (( make_args_string (args) ) if len(args) > 0 else '""'))
+      except IndexError as e:
+        print ("Failed to find documentation of method {0}::{1}".format(cn, mn), file=sys.stderr)
+        line = line.replace (match.group(0), '"' + str(e) + '", ""')
+    print(line, file=ostr)
+
+if __name__ == '__main__':
+  srcfile = sys.argv[1]
+  with open(srcfile) as istr:
+    if len(sys.argv) > 2:
+      dstfile = sys.argv[2]
+      with open(dstfile, "w") as ostr:
+        substitute(istr, ostr)
+    else:
+      substitute(istr, sys.stdout)
---
+++ doc/doxygen_xml_parser.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+from lxml import etree
+from os import path
+import sys
+
+class Index:
+    def __init__ (self, filename):
+        self.tree = etree.parse (filename)
+        self.directory = path.dirname (filename)
+
+    def _getCompound (self, kind, name):
+        return self.tree.xpath("/doxygenindex/compound[@kind='" + kind + "' and name='" + name + "']")[0]
+
+    def classDoc (self, name):
+        el = self._getCompound ("class", name)
+        return ClassDoc (path.join(self.directory, el.get("refid") + ".xml"))
+
+class ClassDoc:
+    def __init__ (self, filename):
+        self.tree = etree.parse (filename)
+        self.compound = self.tree.find ("/compounddef")
+        self.classname = self.compound.find("compoundname").text.strip()
+
+    @staticmethod
+    def _getDoc (el):
+        b = el.find("briefdescription")
+        d = el.find("detaileddescription")
+        return etree.tostring(b, method="text").strip(), d.text.strip()
+
+    def _getMember (self, sectionKind, memberDefKind, name):
+        #return self.compound.xpath ("sectiondef[@kind='" + sectionKind
+        try:
+            return self.compound.xpath ("sectiondef[re:test(@kind,'" + sectionKind
+                    + "')]/memberdef[@kind='" + memberDefKind
+                    + "' and name='" + name + "']",
+                    namespaces={'re': "http://exslt.org/regular-expressions"}
+                    )[0]
+        except IndexError as e:
+            msg = "Error: Could not find member (" + sectionKind + ") " + name + " of class " + self.classname
+            print (msg + "\n" + str(e), file=sys.stderr)
+            raise IndexError (msg)
+
+    def getClassDoc (self):
+        return self._getDoc (self.compound)
+
+    def getClassMemberDoc (self, membername):
+        # member = self.compound.find ("sectiondef[@kind='public-attrib']/memberdef[@kind='variable' and name='" + methodname + "']")
+        member = self._getMember ('public-attrib', 'variable', membername)
+        return self._getDoc(member)
+
+    def getClassMethodDoc (self, methodname):
+        # member = self.compound.xpath ("sectiondef[@kind='public-func']/memberdef[@kind='function' and name='" + methodname + "']")[0]
+        # member = self._getMember ('.*-func', 'function', methodname)
+        member = self._getMember ('.*', 'function', methodname)
+        b, d = self._getDoc(member)
+        dd = member.find("detaileddescription")
+        if member.get("static") == "no": args = ["self",]
+        else                           : args = []
+        args += [ el.text.strip() for el in member.xpath("param/declname") ]
+        for parameters in dd.xpath("para/parameterlist/parameteritem"):
+            pargs = [ el.text.strip() for el in parameters.find("parameternamelist").findall("parametername")]
+            pns = " ".join (pargs)
+            pd = parameters.find("parameterdescription").find("para").text.strip()
+            d += "\n:param " + pns + ":" + pd
+        return b, d, args
+
+# index = Index (sys.argv[1])
+# classDoc = index.classDoc ("hpp::core::BiRRTPlanner")
+# print classDoc.getClassDoc()
+# print classDoc.getClassMethodDoc("oneStep")
