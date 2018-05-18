aa18d4a9857fe553b068980804fedf36cb875854
Joseph Mirabel <jmirabel@laas.fr>
2018-05-18 17:37:37 +0200

    Add some documentation of doc/configure.py and a README.md

--- doc/configure.py
+++ doc/configure.py
@@ -1,4 +1,75 @@
 #!/usr/bin/env python2.7
+#
+# Copyright (c) 2018 CNRS
+# Authors: Joseph Mirabel
+#
+#
+# This file is part of hpp-python
+# hpp-python is free software: you can redistribute it
+# and/or modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, either version
+# 3 of the License, or (at your option) any later version.
+#
+# hpp-python is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Lesser Public License for more details.  You should have
+# received a copy of the GNU Lesser General Public License along with
+# hpp-python  If not, see
+# <http://www.gnu.org/licenses/>.
+
+############# README ###############################
+#
+# This script takes as arguments an input file and an output file.
+#
+# 1. Documentation
+# ----------------------------------------------------------------
+#
+# The following commands will be READ (so they must be commented out to avoid
+# a compilation error):
+#
+# - DocNamespace(namespace)
+#   :param namespace:
+#   Set the name of the current namespace.
+#   This namespace is mapped to a package via variable `nsToPackage`.
+#
+# - DocClass(classname)
+#   :param classname:
+#   Set the name of the current class being documented.
+#
+# The following commands will be REPLACED (so they can be put inplace):
+#
+# - DocClassMethod(methodname,classname)
+#   :param methodname:
+#   :param classname: optional. If not provided, uses the current class name
+#                               defined by DocClass
+#   Extract the documentation of this class method from doxygen XML
+#   documentation of the package associated with the current namespace.
+#
+# 2. Example
+# ----------------------------------------------------------------
+#
+#   The following input
+#   ```cpp
+#   // DocNamespace(hpp::core)
+#   // DocClass(Class)
+#   .def ("function", Class::function, DocClassMethod(function))
+#   ```
+#   will be converted into
+#   ```cpp
+#   // DocNamespace(hpp::core)
+#   // DocClass(Class)
+#   .def ("function", Class::function, "Documentation of Class::function from install/share/doc/hpp-core/doxygen-xml/...", (arg("self"),arg("input1"),...))
+#   ```
+#
+# 2. Current limitation
+# ----------------------------------------------------------------
+#
+# - It is not possible to disambiguate the prototype in case of overloaded function.
+#
+# - The detailed description of the function is not well formatted.
+#
+############# README ###############################
 
 from __future__ import print_function
 import re,os,sys
@@ -13,19 +84,21 @@ class WrongIndex:
 class WrongClassDoc:
     def __init__ (self, msg, name):
         self.msg = msg
-        self.name = name
+        self.classname = name
 
     def _getMsg (self, el):
-        return self.msg + " " + el + " is not documented"
+        msg = self.msg + " " + el + " is not documented"
+        print (msg)
+        return msg
 
     def getClassDoc (self):
-        return self._getMsg(self.name), ""
+        return self._getMsg(self.classname), ""
 
     def getClassMemberDoc (self, member):
-        return self._getMsg(self.name + "::" + member), ""
+        return self._getMsg(self.classname + "::" + member), ""
 
     def getClassMethodDoc (self, method):
-        return self._getMsg(self.name + "::" + method), "", []
+        return self._getMsg(self.classname + "::" + method), "", []
 
 def _xmlDirFromPkgConfig (pkg):
     path = os.popen("pkg-config --variable=docdir " + pkg).read().strip()
