b9e93ba2552696b9a22c6597b0793d8e6746ebc5
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-12-20 10:02:15 +0100

    tests: drop pytest dependency

--- tests/path.py
+++ tests/path.py
@@ -1,26 +1,7 @@
-import pytest
+import unittest
 
-import pyhpp.core
 import numpy as np
-import sys
-
-
-def test_path_api():
-    space = pyhpp.pinocchio.LiegroupSpace.R1(False)
-    path = pyhpp.core.StraightPath.create(
-        space, np.array((0.0,)), np.array((1.0,)), pyhpp.core.interval(1.0, 2.0), None
-    )
-    assert path.initial() == 0.0
-    assert path.end() == 1.0
-    assert path.length() == 1.0
-
-    assert path.eval(1.5) == (0.5, True)
-    assert path.derivative(1.5, 1) == 1.0
-    assert path.derivative(1.5, 2) == 0.0
-
-    extracted_path = path.extract(1.5, 2.0)
-    assert isinstance(extracted_path, pyhpp.core.StraightPath)
-    assert extracted_path.length() == 0.5
+import pyhpp.core
 
 
 class PythonStraightPath(pyhpp.core.PathWrap):
@@ -85,33 +66,53 @@ class PythonStraightPath(pyhpp.core.PathWrap):
         assert self._counters.get(key, 0) > 0
 
 
-def test_path_inheritance():
-    path = PythonStraightPath(2.0, 4.0, 1.0)
+class TestPath(unittest.TestCase):
+    def test_path_api(self):
+        space = pyhpp.pinocchio.LiegroupSpace.R1(False)
+        path = pyhpp.core.StraightPath.create(
+            space,
+            np.array((0.0,)),
+            np.array((1.0,)),
+            pyhpp.core.interval(1.0, 2.0),
+            None,
+        )
+        self.assertEqual(path.initial(), 0.0)
+        self.assertEqual(path.end(), 1.0)
+        self.assertEqual(path.length(), 1.0)
 
-    # path.initial()
-    res, ok = path.eval(1.0)
-    assert ok
-    assert res == 3.0
-    path.assert_called("impl_compute")
+        self.assertEqual(path.eval(1.5), (0.5, True))
+        self.assertEqual(path.derivative(1.5, 1), 1.0)
+        self.assertEqual(path.derivative(1.5, 2), 0.0)
 
-    res = path.derivative(1.0, 1)
-    assert res == 1.0
-    path.assert_called("impl_derivative")
+        extracted_path = path.extract(1.5, 2.0)
+        self.assertTrue(isinstance(extracted_path, pyhpp.core.StraightPath))
+        self.assertEqual(extracted_path.length(), 0.5)
 
-    copy = path.copy()
-    assert copy.initial() == path.initial()
-    assert copy.end() == path.end()
+    def test_path_inheritance(self):
+        path = PythonStraightPath(2.0, 4.0, 1.0)
 
-    # Check that it can be used as a path
-    pv = pyhpp.core.path.Vector.create(1, 1)
-    pv.appendPath(path)
+        # path.initial()
+        res, ok = path.eval(1.0)
+        self.assertTrue(ok)
+        self.assertEqual(res, 3.0)
+        path.assert_called("impl_compute")
 
-    path.clear_counter("copy")
-    _path_0 = pv.pathAtRank(0)
-    path.assert_called("copy")
+        res = path.derivative(1.0, 1)
+        self.assertEqual(res, 1.0)
+        path.assert_called("impl_derivative")
+
+        copy = path.copy()
+        self.assertEqual(copy.initial(), path.initial())
+        self.assertEqual(copy.end(), path.end())
+
+        # Check that it can be used as a path
+        pv = pyhpp.core.path.Vector.create(1, 1)
+        pv.appendPath(path)
+
+        path.clear_counter("copy")
+        _path_0 = pv.pathAtRank(0)
+        path.assert_called("copy")
 
 
 if __name__ == "__main__":
-    args = sys.argv
-    args.append(__file__)
-    sys.exit(pytest.main(args))
+    unittest.main()
