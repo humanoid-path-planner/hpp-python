9070f81255e1d2c480bdeb620ba85dc6f729ce74
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-16 19:31:58 +0100

    flake: Use pinocchio default joint collection

--- flake.nix
+++ flake.nix
@@ -4,6 +4,17 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/362956/head";
+    # we need https://github.com/humanoid-path-planner/hpp-pinocchio/pull/223
+    hpp-core = {
+      url = "github:humanoid-path-planner/hpp-core/devel";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hpp-pinocchio = {
+      url = "github:humanoid-path-planner/hpp-pinocchio/devel";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -11,8 +22,26 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
       perSystem =
-        { pkgs, self', ... }:
         {
+          pkgs,
+          self',
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (_super: prev: {
+                hpp-core = prev.hpp-core.overrideAttrs {
+                  inherit (inputs.hpp-core.packages.${system}.hpp-core) src;
+                };
+                hpp-pinocchio = prev.hpp-pinocchio.overrideAttrs {
+                  inherit (inputs.hpp-pinocchio.packages.${system}.hpp-pinocchio) src;
+                };
+              })
+            ];
+          };
           apps.default = {
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
