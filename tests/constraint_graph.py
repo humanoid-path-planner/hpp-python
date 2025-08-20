ffe988f8c613bcc9cce34cbc38a474917dbf1c1b
Paul Sardin <paulsardin123@gmail.com>
2025-08-20 10:33:10 +0200

    fix import in test file

--- tests/constraint_graph.py
+++ tests/constraint_graph.py
@@ -7,7 +7,7 @@ from pinocchio import SE3, StdVec_Bool as Mask, Quaternion
 =======
 
 from pyhpp.manipulation import Device, urdf, Graph, Problem
-
+from pyhpp.core import ConfigurationShooter
 import numpy as np
 from pinocchio import SE3, StdVec_Bool as Mask, Quaternion
 
