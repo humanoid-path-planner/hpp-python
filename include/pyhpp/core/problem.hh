4a01c7a0e965e88f370746d0c0ac980b7cbb7181
Paul Sardin <paulsardin123@gmail.com>
2025-08-21 13:09:50 +0200

    add steering-method wrapper and update problem

--- include/pyhpp/core/problem.hh
+++ include/pyhpp/core/problem.hh
@@ -31,14 +31,6 @@
 #ifndef PYHPP_CORE_PROBLEM_HH
 #define PYHPP_CORE_PROBLEM_HH
 
-#include <hpp/core/config-validations.hh>
-#include <hpp/core/configuration-shooter.hh>
-#include <hpp/core/distance.hh>
-#include <hpp/core/path-projector.hh>
-#include <hpp/core/path-validation.hh>
-#include <hpp/core/problem-target.hh>
-#include <hpp/core/problem.hh>
-#include <hpp/core/steering-method.hh>
 #include <hpp/pinocchio/device.hh>
 #include <hpp/util/pointer.hh>
 #include <hpp/manipulation/problem.hh>
@@ -46,6 +38,9 @@
 
 namespace pyhpp {
 namespace core {
+
+struct SteeringMethod;
+typedef std::shared_ptr<SteeringMethod> PyWSteeringMethodPtr_t;
 
 typedef hpp::core::ProblemPtr_t ProblemPtr_t;
 typedef hpp::core::ProblemConstPtr_t ProblemConstPtr_t;
@@ -71,14 +66,14 @@ struct Problem {
   const DevicePtr_t& robot() const;
   void setParameter(const std::string& name, const Parameter& value);
   const Parameter& getParameter(const std::string& name) const;
-  SteeringMethodPtr_t steeringMethod() const;
+  PyWSteeringMethodPtr_t steeringMethod() const;
   const ConfigValidationsPtr_t& configValidation() const;
   PathValidationPtr_t pathValidation() const;
   PathProjectorPtr_t pathProjector() const;
   DistancePtr_t distance() const;
   const ProblemTargetPtr_t& target() const;
   ConfigurationShooterPtr_t configurationShooter() const;
-  void steeringMethod(const SteeringMethodPtr_t& sm);
+  void steeringMethod(const PyWSteeringMethodPtr_t& steeringMethod);
   void configValidation(const ConfigValidationsPtr_t& cv);
   void pathValidation(const PathValidationPtr_t& pv);
   void pathProjector(const PathProjectorPtr_t& pp);
