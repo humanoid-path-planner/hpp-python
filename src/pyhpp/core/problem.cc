e787effd73f39b0630f70022727018987c306f5d
Paul Sardin <paulsardin123@gmail.com>
2025-08-21 10:26:01 +0200

    Add pathProjector and pathValidation types and update problem getter and setters

--- src/pyhpp/core/problem.cc
+++ src/pyhpp/core/problem.cc
@@ -51,7 +51,7 @@ const Parameter& Problem::getParameter(const std::string& name) const {
 }
 
 ConfigurationShooterPtr_t Problem::configurationShooter() const {
-  return obj->configurationShooter();
+    return obj->configurationShooter();
 }
 
 SteeringMethodPtr_t Problem::steeringMethod() const {
@@ -74,6 +74,35 @@ DistancePtr_t Problem::distance() const { return obj->distance(); }
 
 const ProblemTargetPtr_t& Problem::target() const { return obj->target(); }
 
+void Problem::configurationShooter(const ConfigurationShooterPtr_t& cs) {
+    obj->configurationShooter(cs);
+}
+
+
+void Problem::steeringMethod(const SteeringMethodPtr_t& sm) {
+    obj->steeringMethod(sm);
+}
+
+void Problem::configValidation(const ConfigValidationsPtr_t& cv) {
+    obj->configValidation(cv);
+}
+
+void Problem::pathValidation(const PathValidationPtr_t& pv) {
+    obj->pathValidation(pv);
+}
+
+void Problem::pathProjector(const PathProjectorPtr_t& pp) {
+    obj->pathProjector(pp);
+}
+
+void Problem::distance(const DistancePtr_t& d) {
+    obj->distance(d);
+}
+
+void Problem::target(const ProblemTargetPtr_t& t) {
+    obj->target(t);
+}
+
 void Problem::initConfig(ConfigurationIn_t inConfig) {
   obj->initConfig(inConfig);
 }
@@ -82,19 +111,75 @@ void Problem::addGoalConfig(ConfigurationIn_t config) {
   obj->addGoalConfig(config);
 }
 
+typedef SteeringMethodPtr_t (Problem::*GetSteeringMethod)() const;
+typedef void (Problem::*SetSteeringMethod)(const SteeringMethodPtr_t&);
+
+typedef const ConfigValidationsPtr_t& (Problem::*GetConfigValidation)() const;
+typedef void (Problem::*SetConfigValidation)(const ConfigValidationsPtr_t&);
+
+typedef PathValidationPtr_t (Problem::*GetPathValidation)() const;
+typedef void (Problem::*SetPathValidation)(const PathValidationPtr_t&);
+
+typedef PathProjectorPtr_t (Problem::*GetPathProjector)() const;
+typedef void (Problem::*SetPathProjector)(const PathProjectorPtr_t&);
+
+typedef DistancePtr_t (Problem::*GetDistance)() const;
+typedef void (Problem::*SetDistance)(const DistancePtr_t&);
+
+typedef const ProblemTargetPtr_t& (Problem::*GetTarget)() const;
+typedef void (Problem::*SetTarget)(const ProblemTargetPtr_t&);
+
+typedef ConfigurationShooterPtr_t (Problem::*GetConfigurationShooter)() const;
+typedef void (Problem::*SetConfigurationShooter)(const ConfigurationShooterPtr_t&);
+
 // Python bindings
 void exposeProblem() {
   class_<Problem>("Problem", init<const DevicePtr_t&>())
       .PYHPP_DEFINE_METHOD_CONST_REF_BY_VALUE(Problem, robot)
       .PYHPP_DEFINE_METHOD(Problem, setParameter)
       .PYHPP_DEFINE_METHOD_CONST_REF_BY_VALUE(Problem, getParameter)
-      .PYHPP_DEFINE_METHOD(Problem, configurationShooter)
-      .PYHPP_DEFINE_METHOD(Problem, steeringMethod)
-      .PYHPP_DEFINE_METHOD_CONST_REF_BY_VALUE(Problem, configValidation)
-      .PYHPP_DEFINE_METHOD(Problem, pathValidation)
-      .PYHPP_DEFINE_METHOD(Problem, pathProjector)
-      .PYHPP_DEFINE_METHOD(Problem, distance)
-      .PYHPP_DEFINE_METHOD_CONST_REF_BY_VALUE(Problem, target)
+      .def("steeringMethod", 
+          static_cast<GetSteeringMethod>(&Problem::steeringMethod))
+      .def("steeringMethod", 
+          static_cast<SetSteeringMethod>(&Problem::steeringMethod),
+          (arg("steeringMethod")))
+      
+      .def("configValidation", 
+          static_cast<GetConfigValidation>(&Problem::configValidation),
+          return_value_policy<copy_const_reference>())
+      .def("configValidation", 
+          static_cast<SetConfigValidation>(&Problem::configValidation),
+          (arg("configValidation")))
+      
+      .def("pathValidation", 
+          static_cast<GetPathValidation>(&Problem::pathValidation))
+      .def("pathValidation", 
+          static_cast<SetPathValidation>(&Problem::pathValidation),
+          (arg("pathValidation")))
+      
+      .def("pathProjector", 
+          static_cast<GetPathProjector>(&Problem::pathProjector))
+      .def("pathProjector", 
+          static_cast<SetPathProjector>(&Problem::pathProjector),
+          (arg("pathProjector")))
+      
+      .def("distance", 
+          static_cast<GetDistance>(&Problem::distance))
+      .def("distance", 
+          static_cast<SetDistance>(&Problem::distance),
+          (arg("distance")))
+      
+      .def("target", 
+          static_cast<GetTarget>(&Problem::target), 
+        return_value_policy<copy_const_reference>())
+      .def("target", 
+          static_cast<SetTarget>(&Problem::target),
+          (arg("target")))
+      .def("configurationShooter", 
+          static_cast<GetConfigurationShooter>(&Problem::configurationShooter))
+      .def("configurationShooter", 
+          static_cast<SetConfigurationShooter>(&Problem::configurationShooter),
+     (arg("configurationShooter")))
       .PYHPP_DEFINE_METHOD(Problem, initConfig)
       .PYHPP_DEFINE_METHOD(Problem, addGoalConfig);
 }
