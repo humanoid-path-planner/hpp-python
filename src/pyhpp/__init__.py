8988da7c73cb8e25cc9f4509577abb81418ee780
Paul Sardin <paulsardin123@gmail.com>
2026-01-19 09:33:49 +0100

    Remove duplicate converter runtime warning

--- src/pyhpp/__init__.py
+++ src/pyhpp/__init__.py
@@ -1,1 +1,9 @@
-import eigenpy  # noqa: F401
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message="to-Python converter .* already registered",
+    category=RuntimeWarning,
+)
+
+import eigenpy  # noqa: E402, F401
