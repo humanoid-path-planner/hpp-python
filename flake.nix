24d1864c678807fb11d8a99bd42cf933fc2f4b85
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-06 21:31:17 +0100

    update packaging

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     gepetto.url = "github:gepetto/nix";
+    gazebros2nix.follows = "gepetto/gazebros2nix";
     flake-parts.follows = "gepetto/flake-parts";
     nixpkgs.follows = "gepetto/nixpkgs";
     nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
@@ -12,26 +13,14 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.gepetto.flakeModule ];
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          apps = lib.filterAttrs (_n: v: v.program.meta.available && !v.program.meta.broken) {
-            default = {
-              type = "app";
-              program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
-            };
-          };
-          packages = {
-            default = self'.packages.hpp-python;
-            hpp-python = pkgs.python3Packages.hpp-python.overrideAttrs (super: {
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = import inputs.systems;
+        imports = [
+          inputs.gepetto.flakeModule
+          {
+            gazebros2nix.overrides.hpp-python = _final: {
               src = lib.fileset.toSource {
                 root = ./.;
                 fileset = lib.fileset.unions [
@@ -43,12 +32,9 @@
                   ./tests
                 ];
               };
-              patches = [ ];
-              propagatedBuildInputs = (super.propagatedBuildInputs or [ ]) ++ [
-                pkgs.python3Packages.hpp-tutorial
-              ];
-            });
-          };
-        };
-    };
+            };
+          }
+        ];
+      }
+    );
 }
