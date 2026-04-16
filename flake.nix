846852c293f4d53903d9d5d32b08a4f8a3f16d23
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-16 12:30:20 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,53 +1,26 @@
 {
   description = "python bindings for HPP, based on boost python";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flakoboros.follows = "gepetto/flakoboros";
-    gazebros2nix.follows = "gepetto/gazebros2nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
-
-    # https://github.com/humanoid-path-planner/hpp-manipulation/pull/262
-    hpp-manipulation.url = "github:humanoid-path-planner/hpp-manipulation";
-    hpp-manipulation.inputs.gepetto.follows = "gepetto";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros = {
-              overlays = [ inputs.hpp-manipulation.overlays.default ];
-              pyOverrideAttrs.hpp-python =
-                _final: python-final:
-                (super: {
-                  propagatedBuildInputs = super.propagatedBuildInputs ++ [
-                    python-final.lxml
-                  ];
-                  src = lib.fileset.toSource {
-                    root = ./.;
-                    fileset = lib.fileset.unions [
-                      ./CMakeLists.txt
-                      ./doc
-                      ./include
-                      ./package.xml
-                      ./src
-                      ./tests
-                    ];
-                  };
-                });
-            };
-          }
-        ];
+        pyOverrideAttrs.hpp-python = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./doc
+              ./include
+              ./package.xml
+              ./src
+              ./tests
+            ];
+          };
+        };
       }
     );
 }
