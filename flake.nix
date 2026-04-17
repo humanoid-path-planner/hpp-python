e5de5be2ca2aec64ff530aa9fb5436a88e57ae4d
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-17 14:10:14 +0200

    nix: include hpp-core#434

--- flake.nix
+++ flake.nix
@@ -1,13 +1,19 @@
 {
   description = "python bindings for HPP, based on boost python";
 
-  inputs.gepetto.url = "github:gepetto/nix";
+  inputs = {
+    gepetto.url = "github:gepetto/nix";
+
+    hpp-core.url = "github:humanoid-path-planner/hpp-core/pull/434/head";
+    hpp-core.inputs.gepetto.follows = "gepetto";
+  };
 
   outputs =
     inputs:
     inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
+        overlays = [ inputs.hpp-core.overlays.flakoboros ];
         pyOverrideAttrs.hpp-python = {
           src = lib.fileset.toSource {
             root = ./.;
