08ff2807a8e5092f45585de2f35c98efedf3c762
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-05-08 11:21:07 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Added input 'hpp-manipulation':
        'github:humanoid-path-planner/hpp-manipulation/88d79cbded13878205bfb89655093163b2aeff0e?narHash=sha256-vbfDlrclTfd2t1O4V4WOmBDSL7DjdJRwMZkYVxUzO0U%3D' (2026-05-08)
    • Added input 'hpp-manipulation/gepetto':
        follows 'gepetto'

--- flake.nix
+++ flake.nix
@@ -4,13 +4,17 @@
   inputs = {
     gepetto.url = "github:gepetto/nix";
 
+    # https://github.com/humanoid-path-planner/hpp-constraints/pull/282 is required
+    hpp-constraints.url = "github:humanoid-path-planner/hpp-constraints";
+    hpp-constraints.inputs.gepetto.follows = "gepetto";
+
     # https://github.com/humanoid-path-planner/hpp-core/pull/434 is required
     hpp-core.url = "github:humanoid-path-planner/hpp-core";
     hpp-core.inputs.gepetto.follows = "gepetto";
 
-    # https://github.com/humanoid-path-planner/hpp-constraints/pull/282 is required
-    hpp-constraints.url = "github:humanoid-path-planner/hpp-constraints";
-    hpp-constraints.inputs.gepetto.follows = "gepetto";
+    # https://github.com/humanoid-path-planner/hpp-manipulation/pull/270 is required
+    hpp-manipulation.url = "github:humanoid-path-planner/hpp-manipulation";
+    hpp-manipulation.inputs.gepetto.follows = "gepetto";
   };
 
   outputs =
@@ -19,8 +23,9 @@
       { lib, ... }:
       {
         overlays = [
-          inputs.hpp-core.overlays.flakoboros
           inputs.hpp-constraints.overlays.flakoboros
+          inputs.hpp-core.overlays.flakoboros
+          inputs.hpp-manipulation.overlays.flakoboros
         ];
         pyOverrideAttrs.hpp-python = {
           src = lib.fileset.toSource {
